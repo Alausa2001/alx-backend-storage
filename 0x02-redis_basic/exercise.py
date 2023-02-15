cba75eb4ff383e049d4ee0552c5d9d711d1265f5
Alausa Abdulqoyum Oluwaferanmi <oluwaferanmialausa2001@gmail.com>
2023-02-16 00:51:10 +0100

    convert to the desired format

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -12,7 +12,7 @@ Type-annotate store correctly. Remember that data can be a
 str, bytes, int or float.
 """
 import redis
-from typing import Union
+from typing import Union, Any, Callable
 from uuid import uuid4
 
 
@@ -31,3 +31,23 @@ class Cache:
         self.key = str(uuid4())
         self._redis.set(self.key, data)
         return self.key
+
+    def get(self, key: str, fn: Callable = None) -> Any:
+        """
+        get method that take a key string argument and an optional Callable
+        argument named fn. This callable will be used to convert the data
+        back to the desired format
+        """
+        if fn:
+            return fn(self._redis.get(key))
+        return self._redis.get(key)
+
+    def get_str(self, key: str) -> str:
+        """return str"""
+        value = self._redis.get(key)
+        return value.decode('utf-8')
+
+    def get_int(self, key: str) -> int:
+        """retrun int"""
+        value = self._redis.get(key)
+        return int.from_bytes(value)
